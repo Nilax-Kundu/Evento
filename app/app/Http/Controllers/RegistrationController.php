661fa95fcd752d4f324d86e5e3670f2f5a315678
Nilax <nilax.root@gmail.com>
2026-05-06 13:12:30 +0530

    fix: import missing Str class in RegistrationController to resolve 500 error

--- app/app/Http/Controllers/RegistrationController.php
+++ app/app/Http/Controllers/RegistrationController.php
@@ -6,6 +6,7 @@ use App\Models\Event;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
 
 class RegistrationController extends Controller
 {
